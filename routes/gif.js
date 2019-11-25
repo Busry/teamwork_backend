d55fd1260fa0d08e0bfa1557f8ce20e470581336
Busry <minitext@gmail.com>
2019-11-25 02:37:59 +0000

    feature(gif):add comment to gif
    [finishes #9]

--- routes/gif.js
+++ routes/gif.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 // const auth = require('../middleware/auth');
 const gifCtrl = require('../controllers/gif');
+const commentCtrl = require('../controllers/comment');
 
 router.post('/', gifCtrl.saveGif);
 router.delete('/:id', gifCtrl.removeGif);
+router.post('/:id/comments', commentCtrl.gifcomment);
 
 module.exports = router;
