b69481bec459b55d5f1d9eadc3878313d4ca8f1f
Busry <minitext@gmail.com>
2020-01-25 21:58:41 +0000

    add x-auth-token to authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied. No token provided' });
+  }
   try {
-    const token = req.headers.authorization.split(' ')[1];
     const { userId } = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    console.log(
-      `userId=>${userId}...req.body.userid=>${req.body.userid}......`
-    );
     if (req.body.userid && req.body.userid !== userId) {
       throw new Error('Invalid user Id');
     } else {
@@ -14,8 +14,8 @@ module.exports = (req, res, next) => {
       next();
     }
   } catch (error) {
-    res.status(401).json({
-      error: new Error('Invalid request '),
+    res.status(400).json({
+      error: new Error(' Invalid token'),
     });
   }
 };
