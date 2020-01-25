a24acc0c71996137e9c816975db407421f9da507
Busry <minitext@gmail.com>
2020-01-25 13:25:00 +0000

    change the pool setting to heroku postgres

--- models/pool.js
+++ models/pool.js
@@ -2,12 +2,18 @@
 const { Pool } = require('pg');
 
 const config = {
-  host: 'localhost',
-  user: 'teamwork',
-  password: 'teamwork2019',
-  database: 'teamwork',
+  host: 'ec2-54-174-229-152.compute-1.amazonaws.com',
+  user: 'rlyehroeuphxdi',
+  password: '5e7177beaeeec315c4bcc9414d28c52ba4cf4d06f05a7d8bc236864a3f80ebd5',
+  database: 'd4998f6tis75f8',
 };
 
+// const config = {
+//   host: 'localhost',
+//   user: 'teamwork',
+//   password: 'teamwork2019',
+//   database: 'teamwork',
+// };
 const pool = new Pool(config);
 
 module.exports = pool;
