3764a0f095f146e6cef183ac575907c7e005de84
Busry <minitext@gmail.com>
2020-01-28 21:05:59 +0000

    revert to manual core setting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileupload = require('express-fileupload');
-const cors = require('cors');
+// const cors = require('cors');
 // Routes
 const articleRoute = require('./routes/article');
 const authRoute = require('./routes/auth');
@@ -14,14 +14,15 @@ const app = express();
 // app setup
 app.use(fileupload({ useTempFiles: true }));
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+
 // set header for CORS access
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, x-auth-token, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 // CRUD
 app.use('/api/v1/auth', authRoute);
